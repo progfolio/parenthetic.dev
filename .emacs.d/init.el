497f08e87d0cf7456f55d1d8057208f095fdef62
progfolio <44036031+progfolio@users.noreply.github.com>
2024-03-17 17:43:41 +0000

    Deploying to gh-pages from @ progfolio/parenthetic.dev@0f2a5d35e8ea6acc98de7269c2cd926a727562c9 🚀

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,17 +1,26 @@
 ;; -*- lexical-binding: t; -*-
-;;(require 'elpaca-installer)
-(require 'build)
+(require 'elpaca-installer)
+(elpaca (mel :host github :repo "progfolio/mel") (require 'mel))
+(elpaca-wait)
 ;;(require 'org)
-
-(setq user-full-name "Nicholas Vollmer"
-      user-mail-address "nv@parenthetic.dev"
-      make-backup-files nil)
-
 ;; (org-link-set-parameters
 ;;  "yt"
 ;;  :follow (lambda (handle) (browse-url (concat "https://www.youtube.com/watch?v=" handle)))
 ;;  :export (lambda (path desc backend channel)
 ;;            (when (eq backend 'html) (build-embedded-video path))))
 
+
+(setq user-full-name "Nicholas Vollmer"
+      user-mail-address "nv@parenthetic.dev"
+      make-backup-files nil)
+
 (message "Building from %s" (expand-file-name user-emacs-directory))
-(build-site)
+
+(with-current-buffer (get-buffer-create "index.html")
+  (erase-buffer)
+  (let ((standard-output (current-buffer))
+        print-circle print-level print-length)
+    (insert
+     "<!DOCTYPE html>\n"
+     (apply #'mel (mel-read "./.emacs.d/parking-page.mel")))
+    (write-file "./index.html")))
